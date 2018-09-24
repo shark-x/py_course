cacb8326dd84f544b9ebbb685f36f2ac55ffeed4
karpeeva_anastasia <charming020793@gmail.com>
2018-09-24 17:21:23 +0700

    редактирование кода относительно PEP8

--- test_add_group.py
+++ test_add_group.py
@@ -2,15 +2,15 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
-class TestAddGroupe(unittest.TestCase):
+class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_groupe(self):
+    def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").clear()
@@ -22,7 +22,7 @@ class TestAddGroupe(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("namegroup")
+        wd.find_element_by_name("group_name").send_keys("name group")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("11111")
@@ -34,13 +34,17 @@ class TestAddGroupe(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
     
     def tearDown(self):
