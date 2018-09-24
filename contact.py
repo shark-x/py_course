949919706604273ed98966c56478da2f705e1891
karpeeva_anastasia <charming020793@gmail.com>
2018-09-24 23:18:27 +0700

    добавлен класс contact создан конструктор класса
    Signed-off-by: karpeeva_anastasia <charming020793@gmail.com>

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, thome, tmobile, twork,
+                 tfax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, group, secaddress, sechome, secnote):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.thome = thome
+        self.tmobile = tmobile
+        self.twork = twork
+        self.tfax = tfax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.group = group
+        self.secaddress = secaddress
+        self.sechome = sechome
+        self.secnote = secnote
