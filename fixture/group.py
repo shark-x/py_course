f6fd20296ba4bb8a258e54b9c54a2dc4837f00a4
karpeeva_anastasia <charming020793@gmail.com>
2018-10-03 15:07:54 +0700

    заполнение формы группы выведенно в отдельную функцию для оптимизации кода,т.к. заполнение формы присутствует в функциях: 1)создания группы 2)изменения группы

--- fixture/group.py
+++ fixture/group.py
@@ -11,22 +11,10 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # Init group creation
         wd.find_element_by_name("new").click()
-        # Fill group form
-        # wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        # wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        # wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation
+        self.fill_form_group(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
-        wd.find_element_by_link_text("groups")
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -42,15 +30,19 @@ class GroupHelper:
         self.open_group_page()
         # selected first group
         wd.find_element_by_name("selected[]").click()
-        # submit edition
         wd.find_element_by_name("edit").click()
+        self.fill_form_group(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+    def fill_form_group(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
-        self.return_to_group_page()
 
     def return_to_group_page(self):
         wd = self.app.wd
