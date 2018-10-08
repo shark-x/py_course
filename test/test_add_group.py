5316244fca1f0515b755ae68cc30468e32261723
karpeeva_anastasia <charming020793@gmail.com>
2018-10-08 11:47:09 +0700

    1. добавлена проверка авторизации/деавторизации пользователя

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="name group", header="11111", footer="22222"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
