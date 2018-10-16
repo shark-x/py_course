b27d18256add9d3264bacf334d4b5582e6b424e0
karpeeva_anastasia <charming020793@gmail.com>
2018-10-16 18:17:55 +0700

    добавлено сравнение списков и сортировка для теста изменения контакта

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,24 +2,24 @@ from model.contact import Contact
 
 
 def test_edit_first_contact_all(app):
+    old_contact_list = app.contact.get_contact_list()
+    contact = Contact(firstname="Nas", middlename="Nasmiddlename", lastname="Naslastname", nickname="Nasnickname")
+    contact.id = old_contact_list[0].id
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="abcd"))
-    app.contact.edit(Contact(firstname="Nas", middlename="Nasmiddlename", lastname="Naslastname", nickname="Nasnickname",
-                            title="Nastitle", company="Nascompany",
-                            address="Nasaddress", thome="Nastelhome", tmobile="Nastelmobile", twork="Nastelwork", fax="fax",
-                            email="Nasemail", email2="Nasemail2", email3="Nasemail3", homepage="Nashomepage",
-                            bday="20", bmonth="November", byear="1991",aday="18", amonth="September", ayear="2016",
-                            secaddress="Nassecaddress", sechome="Nassechome", secnote="Nassecnote"))
-    # app.contact.delete()
+    app.contact.edit(contact)
+    new_contact_list = app.contact.get_contact_list()
+    assert len(old_contact_list) == len(new_contact_list)
+    old_contact_list[0] = contact
+    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
 
-
-def test_edit_first_contact_name(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="abcd"))
-    app.contact.edit(Contact(firstname="edif firstname", middlename="edit middle name", lastname="edit last name"))
-
-
-def test_edit_first_contact_address(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="abcd"))
-    app.contact.edit(Contact(address="edit only address"))
+# def test_edit_first_contact_name(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(firstname="abcd"))
+#     app.contact.edit(Contact(firstname="edif firstname", middlename="edit middle name", lastname="edit last name"))
+#
+#
+# def test_edit_first_contact_address(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(firstname="abcd"))
+#     app.contact.edit(Contact(address="edit only address"))
