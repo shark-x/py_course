ce062aaf3848e145af3a332d8d56aada8aeb1bc2
karpeeva_anastasia <charming020793@gmail.com>
2018-10-16 18:11:53 +0700

    1)переопределена функция сравнения контактов

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, photo=None, title=None, company=None,
@@ -33,3 +36,13 @@ class Contact:
 
     def __repr__(self):
         return ("%s,%s,%s") % (self.firstname, self.lastname, self.id)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname \
+               and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,7 +7,7 @@ def test_add_group(app):
     group = Group(name="name group", header="11111", footer="22222")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
+    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -17,6 +17,6 @@ def test_add_empty_group(app):
     group = Group(name="", header="", footer="")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+ 1 == len(new_groups)
+    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
