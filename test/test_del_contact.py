b4d42eb416aa7a88e021fa370519a1c9eda05f37
karpeeva_anastasia <charming020793@gmail.com>
2018-10-17 11:16:24 +0700

    добавлено сравнение списков в тестах удаления групп и контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -8,4 +8,6 @@ def test_del_first_contact(app):
         app.contact.create(contact)
     app.contact.delete()
     assert len(old_contact_list) - 1 == app.contact.count()
-    # new_contact_list = app.contact.get_contact_list()
+    new_contact_list = app.contact.get_contact_list()
+    old_contact_list[0:1] = []
+    assert old_contact_list == new_contact_list
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,5 +7,7 @@ def test_delete_first_group(app):
     old_groups = app.group.get_group_list()
     app.group.delete_first_group()
     assert len(old_groups)-1 == app.group.count()
-    # new_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    old_groups[0:1] = []
+    assert old_groups == new_groups
 
