1db0011da090dff62f4b4a6564df1354728cbef5
karpeeva_anastasia <charming020793@gmail.com>
2018-10-17 11:49:36 +0700

    оформление кода

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,10 +2,9 @@ from model.contact import Contact
 
 
 def test_del_first_contact(app):
-    old_contact_list = app.contact.get_contact_list()
-    contact = Contact(firstname="FIRSTNAME", tmobile="123456789")
     if app.contact.count() == 0:
-        app.contact.create(contact)
+        app.contact.create(Contact(firstname="FIRSTNAME", tmobile="123456789"))
+    old_contact_list = app.contact.get_contact_list()
     app.contact.delete()
     assert len(old_contact_list) - 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,11 +2,11 @@ from model.contact import Contact
 
 
 def test_edit_first_contact_all(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="abcd"))
     old_contact_list = app.contact.get_contact_list()
     contact = Contact(firstname="Nas", middlename="Nasmiddlename", lastname="Naslastname", nickname="Nasnickname")
     contact.id = old_contact_list[0].id
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="abcd"))
     app.contact.edit(contact)
     assert len(old_contact_list) == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
