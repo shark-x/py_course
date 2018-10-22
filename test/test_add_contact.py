edee85f411e18916c33afe03cee15a9a7007a4ce
karpeeva_anastasia <charming020793@gmail.com>
2018-10-22 10:45:01 +0700

    data driven for test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [
+        Contact(firstname="", middlename="", lastname="")]+[
+        Contact(firstname=random_string("name", 10), middlename=random_string("header", 20), lastname=random_string("footer", 15))
+        for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact(app, contact):
     old_contact_list = app.contact.get_contact_list()
-    contact = Contact(firstname="anasa", middlename="middlename", lastname="lastname")
     app.contact.create(contact)
     assert len(old_contact_list) + 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [Group(name="", header="", footer="")]+[
+        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15))
+        for i in range(5)
+    ]
+
+# test_data = [Group(name=name, header=header, footer=footer)
+#              for name in ["", random_string("name", 10)]
+#              for header in ["", random_string("header", 10)]
+#              for footer in ["", random_string("footer", 10)]
+#              ]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="name group", header="11111", footer="22222")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
